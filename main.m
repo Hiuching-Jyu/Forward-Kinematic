9dd1e5dd92c6889bf063a490a5904c029eed2109
Hiuching-Jyu <Xiaoqing.Zhu19@student.xjtlu.edu.cn>
2023-10-24 15:21:31 +0800

    change the exact value to symbolic values

--- main.m
+++ main.m
@@ -1,14 +1,16 @@
 clc;
 clear;
+format short;
 syms theta1;syms theta2;syms theta3;syms theta4;syms theta5;syms theta6;
+syms a1; syms d2; syms a2; syms a3; syms d4; syms d6;
 
 % calculate the homogeneous transformation matrix
 A1 = four_paramters2matrix(0, 0, 0, theta1);
-A2 = four_paramters2matrix(0.41, -pi/2, 0.78, theta2);
-A3 = four_paramters2matrix(1.075, 0, 0, theta3);
-A4 = four_paramters2matrix(0.165, -pi/2, 1.056, theta4);
+A2 = four_paramters2matrix(a1, -pi/2, d2, theta2);
+A3 = four_paramters2matrix(a2, 0, 0, theta3);
+A4 = four_paramters2matrix(a3, -pi/2, d4, theta4);
 A5 = four_paramters2matrix(0, pi/2, 0, theta5);
-A6 = four_paramters2matrix(0, -pi/2, 0.25, theta6);
+A6 = four_paramters2matrix(0, -pi/2, d6, theta6);
 
 %display the homogeneous transformation matrix
 
@@ -20,14 +22,13 @@ A5
 A6
 
 % display the process of calculation
-
-t01 = A1*A2;
-T12 = A2*A3;
-T23 = A3*A4;
-T34 = A4*A5;
-T45 = A5*A6;
+T02 = A1*A2
+T03 = A1*A2*A3
+T04 = A1*A2*A3*A4
+T05 = A1*A2*A3*A4*A5
+T06 = A1*A2*A3*A4*A5*A6
 
 % Forward kinematic chain
-T = A1*A2*A3*A4*A5*A6;
-disp(T);
+vpa(T06)
+disp(T06);
 
